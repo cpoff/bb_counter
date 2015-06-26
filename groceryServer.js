c82fade449bc3f2f265a2f4822e89b11b6e3a38c
cpoff <curt.poff@gmail.com>
2015-06-26 14:25:55 -0700

    Code clean

--- groceryServer.js
+++ groceryServer.js
@@ -1,41 +1,52 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.use(express.static(__dirname));
+var groceries = [{
+    'name': 'milk',
+    'price': '3',
+    'quant': '6',
+    'id': '0'
+}];
 
-var groceries = [{'name':'milk', 'price':'2', 'quant':'1', 'id':'0'}];
-
-app.get('/groceries/:id', function (req, res) {
+app.get('/groceries/:id', function(req, res) {
     var id = req.params.id;
-    res.send(JSON.stringify({value : groceries[id], groceries:groceries}));
+    res.send(JSON.stringify({
+        value: groceries[id],
+        groceries: groceries
+    }));
 });
 
-app.put('/groceries/:id', function (req, res) {
-    var obj = {name:req.body.name, price:req.body.price, quant:req.body.quant, id:req.body.id};
+app.put('/groceries/:id', function(req, res) {
+    var obj = {
+        name: req.body.name,
+        price: req.body.price,
+        quant: req.body.quant,
+        id: req.body.id
+    };
     var id = req.params.id;
     groceries[id] = obj;
-    console.log(groceries);
-    res.end(JSON.stringify({id:id}))
+    res.end(JSON.stringify({
+        id: id
+    }))
 });
 
-app.get('/groceries', function (req, res) {
+app.get('/groceries', function(req, res) {
     res.send(groceries);
 });
 
-//add destroy route
-app.delete('/groceries/:id', function (req, res) {
+app.delete('/groceries/:id', function(req, res) {
     var id = req.params.id;
     delete groceries[id];
-    console.log(groceries);
     res.end('{}')
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log("server says get some");
 });
-
-//delete http route handler
