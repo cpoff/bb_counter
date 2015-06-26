344ca2156023dead6b58e5c9b8f094bb5fa75bb7
cpoff <curt.poff@gmail.com>
2015-06-26 14:12:09 -0700

    Delete persists

--- groceryServe.js
+++ groceryServe.js
@@ -10,13 +10,17 @@ var GroceryModel = Backbone.Model.extend({
     }
 });
 
+var idCount = 0;
+var viewArr = [];    
+var view;
+
 var GroceryView = Backbone.View.extend({
     render: function () {      
         var valName = this.model.get("name");
         var valPrice = this.model.get("price");
         var valQuant = this.model.get("quant");
-        var btn2 = '<button id=clear>Clear line</button>';
-        var btn = '<button id=delete>Remove last</button>';
+//        var btn2 = '<button id=clear>Clear line</button>';
+        var btn = '<button id=delete>Remove</button>';
         var inputName = '<input type="text" class="inputName" value="' + valName + '" />';
         var inputPrice = '<input type="number" class="inputPrice" value="' + valPrice + '" />';
         var inputQuant = '<input type="number" class="inputQuant" value="' + valQuant + '" />';
@@ -28,8 +32,9 @@ var GroceryView = Backbone.View.extend({
     },
     
     events : {
-        "click #delete" : "delete",
+//        "click #delete" : "delete",
         "keypress input" : "updateOnEnter",
+        "click #delete" : "delete",
         "click #clear" : "clear"
     },
 
@@ -38,6 +43,14 @@ var GroceryView = Backbone.View.extend({
 //        this.model.replace(str);
 //    },
 
+    delete : function () {
+        console.log('Goner');
+        viewArr.pop();
+        this.model.destroy();
+        this.remove();
+        console.log(viewArr);
+    },  
+ 
     nameReplace : function (str) {
         this.model.set('name', str);
         this.model.save();
@@ -50,7 +63,7 @@ var GroceryView = Backbone.View.extend({
 
     quantReplace : function (str) {
         this.model.set('quant', str);
-        console.log(newArr);
+        console.log(viewArr);
         this.model.save();
     },
 
@@ -58,13 +71,14 @@ var GroceryView = Backbone.View.extend({
         this.model.replace("");
         this.model.save();
     },
+//
+//    delete : function () {
+//    console.log('Goner');
+//    var lastItem = viewArr.pop();
+//    lastItem.model.destroy();
+//    console.log(viewArr);
+//    },  
 
-    delete : function () {
-    console.log('Goner');
-    var lastItem = newArr.pop();
-    lastItem.remove();
-    },  
-    
     updateOnEnter: function (e){
         if(e.keyCode == 13) {
             this.nameReplace(this.$el.find(".inputName").val());
@@ -83,33 +97,31 @@ var GroceryCollection = Backbone.Collection.extend({
     }
 });
 
-var idCount = 0;
-var newArr = [];    
-
 var GroceryCollectionView = Backbone.View.extend({
     render : function () {
         var btn = '<button id="addbutton">Add New</button>';
         var div = '<div id="grocery-list"></div>';
         this.$el.html(div + btn);
-        for(var i=0; i < newArr.length; i++) {
-            newArr[i].render();
-            this.$("#grocery-list").append(newArr[i].$el);
+        for(var i=0; i < viewArr.length; i++) {
+            viewArr[i].render();
+            this.$("#grocery-list").append(viewArr[i].$el);
         }
     },
             
     initialize : function () {
         this.listenTo(this.collection, 'add', this.addOne);
-        this.listenTo(this.collection, 'remove', this.delete);
+        this.listenTo(this.collection, 'destroy', this.delete);
     },
+    
     events : {
-        "click #addbutton" : "addCollection",
+        "click #addbutton" : "addCollection"
     },
     
     addOne : function (model) {
         console.log('model added to collection');
 //        model.set("value","Here's where you type something...");
-        var view = new GroceryView({model : model});
-        newArr.push(view);
+        view = new GroceryView({model : model});
+        viewArr.push(view);
         view.render();
         var models = this.collection.models ;
         for(var i=0; i < models.length; i++) {
@@ -118,7 +130,7 @@ var GroceryCollectionView = Backbone.View.extend({
         this.$("#grocery-list").append(view.$el);
         console.log(this.collection.models);
     },
-        
+       
     addCollection : function () {
         this.collection.create({id : idCount});
         idCount = idCount+1;
